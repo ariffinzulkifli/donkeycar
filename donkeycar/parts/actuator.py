6e2820665d94153043ad54880d5e0ebb2ac784e9
ariffinzulkifli <ariffinastute@live.com>
2023-10-23 12:17:39 +0800

    update config

--- donkeycar/parts/actuator.py
+++ donkeycar/parts/actuator.py
@@ -336,22 +336,44 @@ class PWMThrottle:
     MIN_THROTTLE = -1
     MAX_THROTTLE =  1
 
-    def __init__(self, controller=None,
-                    max_pulse=4095,
-                    min_pulse=-4095,
-                    zero_pulse=0):
+    def __init__(self, controller, max_pulse, min_pulse, zero_pulse):
+
+        if controller is None:
+            raise ValueError("PWMThrottle requires a set_pulse controller to be passed")
+        set_pulse = getattr(controller, "set_pulse", None)
+        if set_pulse is None or not callable(set_pulse):
+            raise ValueError("controller must have a set_pulse method")
 
         self.controller = controller
         self.max_pulse = max_pulse
         self.min_pulse = min_pulse
         self.zero_pulse = zero_pulse
-        
-        #send zero pulse to calibrate ESC
-        print("Init ESC")
+        self.pulse = zero_pulse
+
+        # send zero pulse to calibrate ESC
+        logger.info("Init ESC")
+        self.controller.set_pulse(self.max_pulse)
+        time.sleep(0.01)
+        self.controller.set_pulse(self.min_pulse)
+        time.sleep(0.01)
         self.controller.set_pulse(self.zero_pulse)
         time.sleep(1)
+        self.running = True
+        logger.info('PWM Throttle created')
 
-
+    def update(self):
+        while self.running:
+            self.controller.set_pulse(self.pulse)
+    
+    def run_threaded(self, throttle):
+        throttle = utils.clamp(throttle, self.MIN_THROTTLE, self.MAX_THROTTLE)
+        if throttle > 0:
+            self.pulse = dk.utils.map_range(throttle, 0, self.MAX_THROTTLE,
+                                            self.zero_pulse, self.max_pulse)
+        else:
+            self.pulse = dk.utils.map_range(throttle, self.MIN_THROTTLE, 0,
+                                            self.min_pulse, self.zero_pulse)
+            
     def run(self, throttle):
         if throttle > 0:
             pulse = dk.utils.map_range(throttle,
@@ -379,8 +401,9 @@ class PWMThrottle:
             self.controller.pwm.set_pwm(self.controller.channel+6,0,0)
         
     def shutdown(self):
-        self.run(0) #stop vehicle
-
+        # stop vehicle
+        self.run(0)
+        self.running = False
 
 #
 # This seems redundant.  If it's really emulating and PCA9685, then
