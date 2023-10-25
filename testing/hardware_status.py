a24e544a58852027718e7bb6a3428b342c63ec67
ariffinzulkifli <ariffinastute@live.com>
2023-10-25 22:16:43 +0800

    add hardware status

---
+++ testing/hardware_status.py
@@ -0,0 +1,41 @@
+import subprocess
+import psutil
+from datetime import datetime
+
+# Get system date and time
+current_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+
+# Get system uptime
+uptime = subprocess.check_output('cat /proc/uptime', shell=True).decode('utf-8').split()[0]
+epoch_time = float(uptime)
+
+# Get GPU temperature
+gpu_temp = subprocess.check_output("/opt/vc/bin/vcgencmd measure_temp", shell=True).decode("utf-8").strip()
+gpu_temp = gpu_temp.replace("temp=", "")
+
+# Get CPU temperature
+cpu_temp = psutil.sensors_temperatures()['coretemp'][0].current
+
+# Get CPU usage
+cpu_usage = psutil.cpu_percent(interval=1)
+
+# Get RAM usage
+ram_usage = psutil.virtual_memory().percent
+
+# Get Swap usage
+swap_usage = psutil.swap_memory().percent
+
+# Hostname
+hostname = subprocess.check_output('hostname', shell=True).decode('utf-8').strip()
+
+# Format the result
+result = f"Timestamp: {current_time}\n"
+result += f"Epoch Time: {epoch_time}\n"
+result += f"GPU Temperature: {gpu_temp}'C\n"
+result += f"CPU Temperature: {cpu_temp}\n"
+result += f"CPU Usage: {cpu_usage}\n"
+result += f"RAM Usage: {ram_usage}\n"
+result += f"Swap Usage: {swap_usage}\n"
+result += f"Raspberry Pi Hostname: {hostname}"
+
+print(result)
