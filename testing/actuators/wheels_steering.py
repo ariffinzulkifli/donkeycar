bd9fe2f6269a8b92e8b9cdb8846ac2c1596c4586
ariffinzulkifli <ariffinastute@live.com>
2023-10-27 01:34:44 +0800

    update code

--- testing/actuators/wheels_steering.py
+++ testing/actuators/wheels_steering.py
@@ -3,20 +3,6 @@ import Adafruit_PCA9685
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
 
-# Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-
-# Helper function to make setting a servo pulse width simpler.
-def set_servo_pulse(channel, pulse):
-    pulse_length = 1000000    # 1,000,000 us per second
-    pulse_length //= 60       # 60 Hz
-    print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096     # 12 bits of resolution
-    print('{0}us per bit'.format(pulse_length))
-    pulse *= 1000
-    pulse //= pulse_length
-    pwm.set_pwm(channel, 0, pulse)
-
 pwm.set_pwm_freq(60)
 
 try:
