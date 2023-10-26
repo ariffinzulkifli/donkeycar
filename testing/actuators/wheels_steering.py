594037b46636b5a45739f1a9ca22aab3812b603f
ariffinzulkifli <ariffinastute@live.com>
2023-10-27 01:59:31 +0800

    update code

--- testing/actuators/wheels_steering.py
+++ testing/actuators/wheels_steering.py
@@ -1,19 +1,25 @@
 import Adafruit_PCA9685
 
-# Initialise the PCA9685 using the default address (0x40).
+# Initialize the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
 
+# Set the PWM frequency to 60 Hz.
 pwm.set_pwm_freq(60)
 
 try:
     while True:
-        # Move servo based on input
+        # Prompt the user to input a servo angle within the range 280-420.
         steering = int(input('Servo Angle (280-420): '))
+        
+        # Check if the input angle is within the valid range (280-420).
         if 280 <= steering <= 420:
+            # Set the PWM signal for channel 0 to the specified servo angle.
             pwm.set_pwm(0, 0, steering)
         else:
+            # Display an error message for invalid input.
             print('Invalid input! Angle must be between 280 and 420.')
 
 except KeyboardInterrupt:
+    # Handle a keyboard interrupt (Ctrl+C) by setting the servo to a default middle position (350).
     pwm.set_pwm(0, 0, 350)  # Set to middle position
     pass
