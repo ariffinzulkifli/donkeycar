77ec0b294caed5edc435c53f62b9be5ac64b7308
ariffinzulkifli <ariffinastute@live.com>
2023-10-27 00:28:16 +0800

    update code

--- testing/resource_monitoring/hardware_status.py
+++ testing/resource_monitoring/hardware_status.py
@@ -3,18 +3,19 @@ import psutil
 from datetime import datetime
 
 # Get system date and time
-current_time = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+current_time = datetime.now().strftime('%y-%m-%d %H:%M:%S')
 
 # Get system uptime
 uptime = subprocess.check_output('cat /proc/uptime', shell=True).decode('utf-8').split()[0]
 epoch_time = float(uptime)
 
 # Get GPU temperature
-gpu_temp = subprocess.check_output("vcgencmd measure_temp", shell=True).decode("utf-8").strip()
-gpu_temp = gpu_temp.replace("temp=", "")
+gpu_temp = subprocess.check_output('vcgencmd measure_temp', shell=True).decode('utf-8').strip()
+gpu_temp = gpu_temp.replace('temp=', '')
 
 # Get CPU temperature
-cpu_temp = psutil.sensors_temperatures()['coretemp'][0].current
+cpu_temp = subprocess.check_output('cat /sys/class/thermal/thermal_zone0/temp', shell=True).decode('utf-8').strip()
+cpu_temp = cpu_temp / 1000.00
 
 # Get CPU usage
 cpu_usage = psutil.cpu_percent(interval=1)
@@ -29,13 +30,13 @@ swap_usage = psutil.swap_memory().percent
 hostname = subprocess.check_output('hostname', shell=True).decode('utf-8').strip()
 
 # Format the result
-result = f"Timestamp: {current_time}\n"
-result += f"Epoch Time: {epoch_time}\n"
-result += f"GPU Temperature: {gpu_temp}'C\n"
-result += f"CPU Temperature: {cpu_temp}\n"
-result += f"CPU Usage: {cpu_usage}\n"
-result += f"RAM Usage: {ram_usage}\n"
-result += f"Swap Usage: {swap_usage}\n"
-result += f"Raspberry Pi Hostname: {hostname}"
+result = f'Timestamp: {current_time}\n'
+result += f'Epoch Time: {epoch_time}\n'
+result += f'GPU Temperature: {gpu_temp}˚C\n'
+result += f'CPU Temperature: {cpu_temp}\n'
+result += f'CPU Usage: {cpu_usage}\n'
+result += f'RAM Usage: {ram_usage}\n'
+result += f'Swap Usage: {swap_usage}\n'
+result += f'Raspberry Pi Hostname: {hostname}'
 
 print(result)
